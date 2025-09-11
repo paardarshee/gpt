ae786959bbabd273a4f82a6cd6b94ea5cba22cc9
Niraj Modi <modiniraj1034@gmail.com>
2025-09-11 10:43:35 +0530

    fixed screen dispositioning

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 export default function TopBar() {
   const { toggleSidebar } = useAppStore();
   return (
-    <div className="flex w-screen items-center justify-between p-2 text-white md:hidden">
+    <div className="flex items-center justify-between p-2 text-white">
       {/* Hamburger */}
       <button
         onClick={toggleSidebar}
-        className="box-border rounded-md border-2 border-transparent p-1 focus:border-white"
+        className="box-border rounded-md border-2 border-transparent p-1 focus:border-white md:hidden"
       >
         <HamBurgerMenu className="h-6 w-6" />
       </button>
@@ -18,7 +18,7 @@ export default function TopBar() {
         {/* Share */}
         <Link
           href="/"
-          className="items-centerrounded-md flex p-2 hover:bg-gray-600"
+          className="items-centerrounded-md flex p-2 hover:bg-gray-600 md:hidden"
         >
           <Create className="h-5 w-5" />
         </Link>
